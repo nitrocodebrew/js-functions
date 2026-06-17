16cb749e0eb8d44ede6022e63e3265f25e479654
John Smicklo <204639360+nitrocodebrew@users.noreply.github.com>
2026-06-17 09:36:27 -0400

    Fix indexing error on input_str
    Indexing got me; fix incorrect argument for add7()

--- js/script.js
+++ js/script.js
@@ -8,16 +8,16 @@ function multiply(num_one, num_two) {
 
 function capitalize(input_str) {
     let new_string = input_str[0].toUpperCase();
-    new_string += input_str.substring(1, (input_str.length - 1)).toLowerCase();
+    new_string += input_str.substring(1, input_str.length).toLowerCase();
     console.log(new_string);    
 }
 
 function lastLetter(input_str) {
-    console.log(input_str[(input_str.length - 1)]);
+    console.log(input_str[input_str.length]);
 }
 
 // INVOKE FUNCTIONS
-add7(7); // Should be 17
+add7(10); // Should be 17
 multiply(3, 2); // Should be 6
 
 ////
